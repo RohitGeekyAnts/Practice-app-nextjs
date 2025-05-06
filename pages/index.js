fe969644d0336f4495ee052a985bda11d1cf7d88
Rohit Kumar <poni2243548@gmail.com>
2025-05-06 13:55:56 +0530

    Import for Home css removed

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import styles from "../styles/home.module.css";
+
 import img from "../public/img-2.jpg";
 
 const Home = () => {
